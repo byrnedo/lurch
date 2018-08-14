e6bf51364efa2bc4bb63afbb1bec9b94427df135
Donal Byrne <byrnedo@tcd.ie>
2018-08-14 17:55:29 +0200

    Now creates docker network

--- test/test.sh
+++ test/test.sh
@@ -26,10 +26,17 @@ $DIR/scripts/make-cert.sh foo.bar2 www.foo.bar2
 
 cd -
 
+echoH "Creating user defined network"
+set +e
+docker network create test-openresty-proxy
+set -e
+
 APPS_JSON=$(cat ./apps.json)
 echoH "Running Dgoss"
 
+
 dgoss run --rm -it \
+    --network test-openresty-proxy \
     --add-host foo.bar:127.0.0.1 \
     --add-host foo.bar2:127.0.0.1 \
     --add-host www.foo.bar:127.0.0.1 \
