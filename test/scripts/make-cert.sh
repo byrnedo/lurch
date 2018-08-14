373b9660c652e5472d3d8589317c9518502d4ad5
Donal Byrne <byrnedo@tcd.ie>
2018-08-14 17:46:18 +0200

    Now tests basic routing

--- test/scripts/make-cert.sh
+++ test/scripts/make-cert.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
 if [ $# -lt 1 ]; then
-    echo >&1 "usage: $0 DOMAIN"
+    echo >&1 "usage: $0 DOMAIN [DOMAIN2] [DOMAIN3]"
     exit 1
 fi
 
 DOMAIN=${1}
+
+SANS=()
+for i in "$@"; do 
+    SANS+=("DNS:${i}")
+done
+
+SAN_JOINED=$(join_by "," "${SANS[@]}")
+
 set +e 
 mkdir -p ./$DOMAIN
 set -e
-openssl req -new -x509 -nodes -out ./$DOMAIN/server.crt -keyout ./$DOMAIN/server.key -subj "/C=SE/ST=/L=Gothenburg/O=FoobarInc Network/OU=IT Department/CN=$DOMAIN"
+echo $SAN_JOINED
+openssl req -new -x509 -reqexts SAN -nodes -out ./$DOMAIN/server.crt -keyout ./$DOMAIN/server.key -subj "/C=SE/L=Gothenburg/O=FoobarInc Network/OU=IT Department/CN=$DOMAIN" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "\n[SAN]\nsubjectAltName=$SAN_JOINED")) 
--- test/test.sh
+++ test/test.sh
@@ -5,8 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 #curl -L https://github.com/aelsabbahy/goss/releases/download/v0.3.6/goss-linux-amd64 -o ~/Downloads/goss-linux-amd64
 
-#export GOSS_PATH=~/Downloads/goss-linux-amd64
-
+#export GOSS_PATH=~/Downloads/goss-linux-amd64 
 function echoH() {
 echo
 echo "########################"
@@ -22,13 +21,20 @@ set -e
 cd ./tmp/certs
 
 echoH "Making test cert"
-$DIR/scripts/make-cert.sh foo.bar
-$DIR/scripts/make-cert.sh foo.bar2
+$DIR/scripts/make-cert.sh foo.bar www.foo.bar
+$DIR/scripts/make-cert.sh foo.bar2 www.foo.bar2
 
 cd -
 
+APPS_JSON=$(cat ./apps.json)
 echoH "Running Dgoss"
-dgoss run \
+
+dgoss run --rm -it \
+    --add-host foo.bar:127.0.0.1 \
+    --add-host foo.bar2:127.0.0.1 \
+    --add-host www.foo.bar:127.0.0.1 \
+    --add-host www.foo.bar2:127.0.0.1 \
+    -e APPS_CONFIG_JSON="$APPS_JSON" \
     -v $PWD/tmp/certs/foo.bar:/usr/local/openresty/nginx/ssl/foo.bar/ \
     -v $PWD/tmp/certs/foo.bar2:/usr/local/openresty/nginx/ssl/foo.bar2/ \
     test
