a384caaef988560e8888593beb83060c86d094f7
Donal Byrne <byrnedo@tcd.ie>
2023-04-17 08:38:11 +0200

    Remove unused error sections

--- lua/error_page.lua
+++ lua/error_page.lua
@@ -5,18 +5,18 @@ local errors_map = {
         title = "Something went wrong",
         message = "We're very sorry for any inconvenience, our service team has been notified."
     },
-    [502] = {
-        title = "Something went wrong",
-        message = "We're very sorry for any inconvenience, our service team has been notified."
-    },
-    [503] = {
-        title = "Something went wrong",
-        message = "We're very sorry for any inconvenience, our service team has been notified."
-    },
-    [504] = {
-        title = "Something went wrong",
-        message = "We're very sorry for any inconvenience, our service team has been notified."
-    },
+    --[502] = {
+    --    title = "Something went wrong",
+    --    message = "We're very sorry for any inconvenience, our service team has been notified."
+    --},
+    --[503] = {
+    --    title = "Something went wrong",
+    --    message = "We're very sorry for any inconvenience, our service team has been notified."
+    --},
+    --[504] = {
+    --    title = "Something went wrong",
+    --    message = "We're very sorry for any inconvenience, our service team has been notified."
+    --},
     [404] = {
         title = "That site doesn't seem to exist",
         message = "Sorry, but there's nothing to see."
